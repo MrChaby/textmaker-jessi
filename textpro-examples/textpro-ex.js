49230940bc041ecf285673c52b265bf2782b9b5b
BuDy X <85840062+MrChaby@users.noreply.github.com>
2021-08-04 18:23:51 +0530

    Textmaker-For-Jessi
    @phaticusthiccy
    ); https://github.com/phaticusthiccy/TextMaker-Unlimited.git

---
+++ textpro-examples/textpro-ex.js
@@ -0,0 +1,116 @@
+const jesssapi = require('textmaker-jess'); // Import NPM Package
+
+// Some Examples
+
+jesssapi.textpro("https://textpro.me/create-realistic-vintage-style-light-bulb-1000.html",
+    ["textmaker"], ["jessi"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/create-cool-wall-graffiti-text-effect-online-1009.html",
+    ["textmaker"], ["jessi"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/create-3d-avengers-logo-online-974.html",
+    ["textmaker"], ["jessi"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/create-logo-style-marvel-studios-ver-metal-972.html",
+    ["textmaker"], ["jessi"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/create-logo-style-marvel-studios-online-971.html",
+    ["textmaker"], ["jessi"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/pornhub-style-logo-online-generator-free-977.html",
+    ["textmaker"], ["jessi"]
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/thunder-text-effect-online-881.html",
+    "jessi"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/advanced-glow-text-effect-873.html",
+    "jessi"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/create-a-futuristic-technology-neon-light-text-effect-1006.html",
+    "jessi"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/bokeh-text-effect-876.html",
+    "jessi"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/neon-light-text-effect-with-galaxy-style-981.html",
+    "jessi"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+jesssapi.textpro("https://textpro.me/neon-light-text-effect-online-882.html",
+    "jessi"
+    ).then(async (data) => { 
+      try { 
+          console.log(data)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+
+/*
+ And Nearly all the others support :)
+*/
